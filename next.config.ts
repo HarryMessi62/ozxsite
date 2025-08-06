0f93ff0e4e523e3482993e4d5faa42da7c25f33e
root <root@coffee-moon.yeezyhost.net>
2025-08-06 09:01:56 +0200

    updt

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   experimental: {
     esmExternals: true,
   },
+   eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
